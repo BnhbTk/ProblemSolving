c3c83f4c83ee2774d55a3b40996067e5a7cae143
BnhbTk <53897578+BnhbTk@users.noreply.github.com>
2025-04-29 23:03:50 +0100

    Update sort_list.py

--- Chapter3/sort_list.py
+++ Chapter3/sort_list.py
@@ -1,5 +1,8 @@
+# This is un update of the first version, now with the three fitness functions
+
 import math
 import random
+import sys
 
 
 class Sorting_List:
@@ -22,6 +25,13 @@ class Sorting_List:
         return f"{'Sorted' if self.is_sorted() else 'Unsorted'}"
     
     def compute_fitness(self):
+        def penalty():
+            sz=len(self.elements)
+            m=0
+            for i in range(sz):
+                m+=len([1 for x in self.elements[:i] if x>self.elements[i]])+len([1 for x in self.elements[i+1:] if x<self.elements[i]])
+            self.fitness=m
+        
         def longest_sequence():
             l=0
             i=1
@@ -37,18 +47,23 @@ class Sorting_List:
             if l<cur:l=cur
             self.fitness=sz-l
         
-        return longest_sequence()
+        def good_orders():
+            self.fitness=sum([0 if self.elements[i-1]<=self.elements[i] else 1 for i in range(1,len(self.elements))])
         
+        {"pen":penalty,"longest":longest_sequence,"order":good_orders}[Sorting_List.method]()
+    
     def __lt__(self,e):
         return self.fitness<e.fitness
     
     def mutate(self):
-        i=random.randrange(0,len(self.elements))
-        while True:
-            j=random.randrange(0,len(self.elements))
-            if i!=j:break
-        if i < j and self.elements[i]>self.elements[j]:
-            self.elements[i],self.elements[j]=self.elements[j],self.elements[i]
+        pm=0.1#1/len(self.elements)
+        for i in range(len(self.elements)):
+            if random.random()<pm:
+                while True:
+                    j=random.randrange(0,len(self.elements))
+                    if i!=j:break
+                if i < j and self.elements[i]>self.elements[j]:
+                    self.elements[i],self.elements[j]=self.elements[j],self.elements[i]
         self.compute_fitness()
         return self
     
@@ -57,43 +72,52 @@ class Sorting_List:
         res.elements=self.elements[:]
         return res
     
-    def local_search(self,neighbors):
+    def local_search(self,neighbors,method):
         current=self
+        Sorting_List.method=method
         current.compute_fitness()
         it=0
         while True:
             if current.is_sorted():break
             it+=1
-            if it%25==0:print(it,current.fitness)
+            print(it,current.fitness)
             best_neighbor=min([current.clone().mutate() for i in range(neighbors)])
             if current>best_neighbor:
                 current=best_neighbor
             else:
                 break
+        print("\n")
         return current
 
-    def local_search_2nd_chance(self,neighbors,max_iter):
+    def local_search_2nd_chance(self,neighbors,max_iter,method):
         print("Local search 2nd chance")
         current=self
+        Sorting_List.method=method
         current.compute_fitness()
         for it in range(max_iter):
-            if it%25==0:print(it,current.fitness)
+            sys.stdout.write(f"\r{it} {current.fitness}   ")
+            sys.stdout.flush()
             best_neighbor=min([current.clone().mutate() for _ in range(neighbors)])
             if current>best_neighbor:
                 current=best_neighbor
             if current.fitness==0:
                 current.compute_fitness()
             if current.is_sorted():
+                sys.stdout.write(f"\r{it} {current.fitness}   ")
+                sys.stdout.flush()
                 break
+        print("")
         return current
     
-    def simulated_annealing(self,neighbors,max_iter,T=20,alpha=0.95):
+    def simulated_annealing(self,neighbors,max_iter,method,T=20,alpha=0.95):
         print("Simulated annealing")
         current=self
         best=current
+        Sorting_List.method=method
         current.compute_fitness()
         for it in range(max_iter):
-            if it%25==0:print(f"{it} {best.fitness}")
+            sys.stdout.write(f"\r{it} {best.fitness}   ")
+            sys.stdout.flush()
             best_neighbor=min([current.clone().mutate() for _ in range(neighbors)])
             if current>best_neighbor:
                 current=best_neighbor
@@ -104,15 +128,20 @@ class Sorting_List:
             if current<best:
                 best=current
             if best.is_sorted():
+                sys.stdout.write(f"\r{it} {best.fitness}   ")
+                sys.stdout.flush()
                 break
             T*=alpha
+        print("")
         return best
+
+
+
     
 l=Sorting_List(100)
 l.generate_problem()
 lp=Sorting_List(0,False)
 lp.elements=l.elements[:]
 print(f"Initial problem: {l}")
-print(f"Solution is {l.local_search(20)}\n")
-print(f"Solution is {l.local_search_2nd_chance(20,2000)}\n")
-print(f"Solution is {lp.simulated_annealing(20,2000)}\n")
+print(f"Solution is {l.local_search_2nd_chance(20,2000,'pen')}\n")
+print(f"Solution is {lp.simulated_annealing(20,2000,'pen')}\n")
